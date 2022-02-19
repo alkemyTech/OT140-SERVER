475dfbd6353bfa9576e33b71f7d430221d0f778b
unknown <ezequielrango@hotmail.com>
2022-02-19 19:21:34 -0300

    Creation of a pathway responsible for updating activities

---
+++ routes/activities.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const activitiesController = require('../controllers/activities.js');
+
+
+router.put('/:id', activitiesController.update)
