57272f13483a41a94413dc41b56d8200ddff5447
unknown <ezequielrango@hotmail.com>
2022-02-21 23:03:17 -0300

    add middleware CheckIsAdmin

--- routes/activities.js
+++ routes/activities.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const activitiesController = require('../controllers/activities.js');
+const checkIsAdmin = require('../middlewares/role');
 
-
-router.put('/:id', (req,res)=> {
-    activitiesController.update
-})
+router.put('/:id',checkIsAdmin,activitiesController.update);
 
 module.exports = router;
