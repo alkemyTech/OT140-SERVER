c41037512b5eeb093d8e5115ff5ce745d1abf191
Jeremias <jnahuel46@gmail.com>
2022-02-21 23:55:54 -0300

    prueba branch 2

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. PRUEBA*/
+/* GET users listing. PRUEBA/prueba branch*/
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
