c140873217a202c2b6ef995e3ed5db42715c90dc
Artemio <artemiolucero@gmail.com>
2022-02-24 23:12:18 -0300

    Added function sendWelcomeEmail to POST new user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { sendWelcomeEmail } = require("../middlewares/sendEmail");
+
+/* POST create new users
+//router.post('/register'...............)
+//once created and assigned the database id to new user
+//call the function sendWelcomeEmail() 
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
