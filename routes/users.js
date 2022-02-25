99d3253fe258a8eeaaee6d71113acfbce5be5aa1
alesandroo92 <alesandro_92@hotmail.com>
2022-02-25 10:17:52 -0300

    User update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var userController = require("../controllers/userController");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.put("/:id", userController.userUpdate);
+
 
 module.exports = router;
