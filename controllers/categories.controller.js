4e6e4aa0d235876acb32c1a6ca3988276f36a9f6
burdiaktri <burdiaktrinidad33@gmail.com>
2022-02-26 13:50:25 -0300

    get category by id corrected

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,15 +1,5 @@
 const { Categories } = require("../models/index");
 
-const getCategories = async (req, res, next) => {
-  try {
-    const categories = await Categories.findAll();
-    categories
-      ? res.status(200).send(categories)
-      : res.status(404).send("Categories not found");
-  } catch (err) {
-    console.log(err);
-  }
-};
 
 const getCategoryById = async (req, res, next) => {
   try {
@@ -29,61 +19,7 @@ const getCategoryById = async (req, res, next) => {
   }
 };
 
-const createCategories = async (req, res, next) => {
-  try {
-    const { name, description, image } = req.body;
-    const categories = new Categories({
-      name,
-      description,
-      image,
-    });
-    const newCategory = await Categories.create(categories);
-    res
-      .status(200)
-      .send({ data: newCategory, message: "Category succesfully created" });
-  } catch (err) {}
-};
 
-const updateCategories = async (req, res, next) => {
-    try{
-        const {id} = req.params;
-        const {name, description, image} = req.body;
-        const category = await Categories.findAll({
-            atributtes: ['name', 'description', 'image'],
-            where: {
-                id
-            }
-        })
 
-        if(category.length > 0 ){
-            category.forEach(async (cat) => {
-                await cat.update({
-                    name,
-                    description,
-                    image
-                })
-            })
-        }
-        res.send("Succesfully updated")
 
-    }catch(err){
-console.log(err)
-    }
-}
-
-const deleteCategories = async (req, res, next) => {
-    const {id} = req.params;
-        const {name, description, image} = req.body;
-        try{
-            const category = await Categories.destroy({
-                where: {
-                    id
-                }
-            })
-            res.send('Succesfully deleted')
-
-        }catch(err){
-console.log(err)
-        }
-}
-module.exports = { getCategoryById, getCategories, createCategories, updateCategories, deleteCategories };
+module.exports = { getCategoryById};
--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const categoriesRouter = express.Router()
-const {getCategoryById, getCategories, createCategories, updateCategories, deleteCategories} = require('../controllers/categories.controller')
+const {getCategoryById} = require('../controllers/categories.controller')
 
 categoriesRouter
             .get('/:id', getCategoryById)
-            .get('/', getCategories)
-            .post('/', createCategories)
-            .put('/:id', updateCategories)
-            .delete('/:id', deleteCategories)
+  
   module.exports = categoriesRouter;
