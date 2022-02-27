48050475cfe3176b6d224ca75df1d35117c31260
Artemio <artemiolucero@gmail.com>
2022-02-27 19:29:50 -0300

    Modified User model and migration.

--- models/user.js
+++ models/user.js
@@ -1,29 +1,30 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      User.belongsTo(models.Role, {as: 'role'});
-    }
-  };
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    image: DataTypes.STRING,
-    password: DataTypes.STRING,
-    roleId: DataTypes.INTEGER,
-    deletedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            User.belongsTo(models.Role, { as: 'role' });
+        }
+    };
+    User.init({
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        photo: DataTypes.STRING,
+        roleId: DataTypes.INTEGER,
+        isActive: DataTypes.BOOLEAN,
+        deletedAt: DataTypes.DATE
+    }, {
+        sequelize,
+        modelName: 'User',
+    });
+    return User;
 };
