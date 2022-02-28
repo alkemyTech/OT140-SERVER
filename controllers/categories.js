c3fcefa9becae291fee1040bf780fe484bf5dce2
Artemio <artemiolucero@gmail.com>
2022-02-28 16:25:59 -0300

    Added get categories paginate

--- controllers/categories.js
+++ controllers/categories.js
@@ -3,10 +3,21 @@ const { Categories } = require('../models/index');
 
 const getCategories = async(req, res) => {
 
+    let limit = 10;
+    let offset = 0;
+    const data = await Categories.findAndCountAll();
+    let page = req.params.page; // page number
+    let pages = Math.ceil(data.count / limit);
+    offset = limit * (page - 1);
+
+    const previousPage = page - 1;
+    const nextPage = page + 1;
+
     const categorias = await Categories.findAll({
         attributes: ['name']
     });
-    res.json(categorias);
+
+    res.status(200).json({ 'result': categorias, 'count': data.count, 'pages': pages, 'previousPage': previousPage, 'nextPage': nextPage });
 };
 
 const updateCategories = async(req, res) => {
