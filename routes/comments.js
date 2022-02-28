91ca6846b20424cf5533bf57559d704fee35d06f
unknown <ezequielrango@hotmail.com>
2022-02-28 05:23:56 -0300

    apply middleware validator in the update route

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,7 @@ const commentsController = require('../controllers/comments');
 const {validateComment} = require('../middlewares/commentValidator');
 // const isOwnerBy = require('../middlewares/owner'); // Middleware para determinar si el usuario es el autor del comentario
 
-router.put('/:id',/*isOwnerBy*/commentsController.update);
+router.put('/:id',validateComment,/*isOwnerBy*/commentsController.update);
 router.delete('/:id',commentsController.remove);
 router.post('/',validateComment,commentsController.create);
   
