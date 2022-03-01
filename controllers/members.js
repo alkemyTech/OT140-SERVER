f9d302a57d767b4a6b66ba2f29409a1c37ef9408
Artemio <artemiolucero@gmail.com>
2022-03-01 00:18:42 -0300

    Added endpoint POST members and create controller.

--- controllers/members.js
+++ controllers/members.js
@@ -17,6 +17,26 @@ const getAll = async(req, res) => {
     };
 };
 
+const create = async(req, res) => {
+    try {
+        const { name } = req.body;
+
+        if (typeof name === "string") {
+            const newMember = await Members.create({ name: name });
+            res.status(200).json({
+                sucess: true,
+                msg: `Member ${name} was created.`,
+                newMember
+            });
+        }
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            msg: `The member name isn't a string.`
+        });
+    };
+};
 module.exports = {
-    getAll
+    getAll,
+    create
 }
--- routes/members.js
+++ routes/members.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAll } = require('../controllers/members');
+const { getAll, create } = require('../controllers/members');
 
 /* GET members listing. */
 router.get('/', getAll);
+
+/* POST members listing. */
+router.post('/', create);
 
 module.exports = router;
