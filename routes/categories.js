ae9bda2b4cee327dcee3dfc2cc7957aea31f4b3a
Jeremias <jnahuel46@gmail.com>
2022-03-05 05:38:19 -0300

    rutas protegidas

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getCategories, updateCategories } = require('../controllers/categories');
-
+const {checkToken} = require('../middlewares/check-token');
 
 
 router.get('/', getCategories);
-router.put('/:categorieId', updateCategories);
+router.put('/:categorieId',checkToken, updateCategories);
 
 module.exports = router;
