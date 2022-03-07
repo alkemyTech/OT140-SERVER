5ccf1403742ee168a7e03328bfaadd7b8566cdd0
unknown <ezequielrango@hotmail.com>
2022-03-07 17:07:23 -0300

    create ownComment function

--- middlewares/owner.js
+++ middlewares/owner.js
@@ -1,31 +1,25 @@
 const db = require("../models");
+const {checkToken} = require('./check-token'); 
+
+// No funciona, todavia no tengo autenticación de usuarios ni la funcion que genera token
 
 const isOwnComment = async (req, res, next) => {
   try {
-
-    const reqId = tokenId(req); // No tengo middleware JWT. id contenido en el token. Por el momento dice que no está definido.
+    
+    const userDecoded = checkToken();
     const { id } = req.params;
-    const userFound = await db.User.getById(reqId);
+    const author = await db.User.getById(userDecoded.id);
 
-    if (!userFound) {
+    if (!author) {
        res.status(404).json('User not found');
-    };
-
-    const role = await db.Role.getByName('Admin');
-    if (!role) {
-      res.status(404).json('Role not found');
-    };
-
-    if (userFound.roleId === role.id) {
-      return next();
     };
 
     const comment = await db.Comment.getById(id);
 
-    if (comment.UserId === userFound.id) {
+    if (comment.UserId === author.id) {
       return next();
     }else{
-      res.status(403).json('You are not the author of the comment or you are not an administrator.')
+      res.status(403).json('you are not the author of this comment')
     }
 
   } catch (err) {
--- routes/comments.js
+++ routes/comments.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const commentsController = require('../controllers/comments');
 const {validateComment} = require('../middlewares/commentValidator');
-// const isOwnerBy = require('../middlewares/owner'); // Middleware para determinar si el usuario es el autor del comentario
+/*const isOwnerBy = require('../middlewares/owner');*/// Middleware para determinar si el usuario es el autor del comentario
 
-router.put('/:id',validateComment,/*isOwnerBy*/commentsController.update);
+router.put('/:id',validateComment,/*isOwnerBy,*/commentsController.update);
 router.delete('/:id',commentsController.remove);
 router.post('/',validateComment,commentsController.create);
   
