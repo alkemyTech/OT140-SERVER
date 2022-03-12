abc24cc8b418f41581a77aa0c755fed70eabf4bb
Jeremias <jnahuel46@gmail.com>
2022-03-12 17:01:06 -0300

    OT-140-60 Update Slides

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const categoriesRouter = require("./routes/categories");
 const organizationsRouter = require("./routes/organizations");
 const testimonialsRouter = require("./routes/testimonials");
 const newsRouter = require("./routes/news");
+const slidesRouter = require("./routes/slides");
 
 const app = express();
 app.use(cors());
@@ -36,6 +37,7 @@ app.use("/categories", categoriesRouter);
 app.use("/organizations", organizationsRouter);
 app.use("/testimonials", testimonialsRouter);
 app.use("/news", newsRouter);
+app.use("/slides", slidesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
