91a26e5640f1e628598bf12c4e2e996f9feccda1
Ricardo <ricardoemanuelracca@gmail.comgit config --global user.email ricardoemanuelracca@gmail.comgit config --global user.email ricardoemanuelracca@gmail.com>
2022-04-05 16:13:43 -0300

    fixed associations news/categories and repeated migra_file

--- models/categories.js
+++ models/categories.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
+            Categories.hasMany(models.New, { 
+                foreignKey: 'categoryId' 
+            })
         }
     };
     Categories.init({
--- models/new.js
+++ models/new.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      New.belongsTo(models.Role, {as: 'categories'});
+      New.belongsTo(models.Categories, {
+        foreignKey: 'categoryId', 
+        targetKey: 'id'
+      });
     }
   };
   New.init({
