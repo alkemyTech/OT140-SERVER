5ebbd12e12ded936b6fe774f15cdc1f8f78dd6e8
javier1793-op <javier17unt@gmail.com>
2022-04-06 20:48:44 -0300

    middleare_admin

--- middlewares/check-token.js
+++ middlewares/check-token.js
@@ -1,31 +1,22 @@
 
-// ====================================
-// Verifica TOKEN en el Header
-// ====================================
-const jwt = require("jsonwebtoken");
-const { User } = require("../models/");
+const jwt = require ('jwt-simple');
+const moment = require ('moment');
 
-const checkToken = async(req, res, next) => {
-    if (!req.headers['user-token']) {
-        return res.status(401).json({
-            error: 'Need to include user-token on the header'
-        });
+const checkToken = (req, res, next) => {
+    if (!req.headers['user-token']){
+        return res.json ({error: 'You need to include the user-token in the header'});
     }
+    
+    const userToken = req.headers['user-token'];
+    let payload={};
+
     try {
-        const userToken = req.headers['user-token'];
-        const { uid } = jwt.verify(userToken, process.env.SECRET);
-        const user = await User.findOne({ where:{ id : uid, deletedAt : null } });
-        if(!user) return res.status(404).json({ msg: 'Invalid token - User not found' });
-        req.user = user;
-        next();
-        
+        payload = jwt.decode(userToken, 'SECRETO');
     } catch (error) {
-        return res.status(401).json({
-            error: 'Error- Wrong TOKEN'
-        });
+        return res.json({error: 'The token is wrong'});
     }
+        next();
+
 }
 
-module.exports = {
-    checkToken,
-};
+module.export =  checkToken
