d976488f48a45c5a90155eb7f3a1dd59ac3d7353
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2022-04-06 23:44:28 -0300

    Added relationship user-role models

--- models/user.js
+++ models/user.js
@@ -2,37 +2,38 @@ const bcrypt = require("bcrypt");
 'use strict';
 
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      User.belongsTo(models.Role, {as: 'role'});
-    }
-  };
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    image: DataTypes.STRING,
-    password: {
-      type: DataTypes.STRING,
-      set(value) {
-      const hash = bcrypt.hashSync(value, 10);
-      this.setDataValue('password', hash);
-    }},
-    roleId: DataTypes.INTEGER,
-    deletedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'User',
-    timestamps: true,
-    paranoid: true
-  });
-  return User;
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            User.belongsTo(models.Role, { as: 'roleId' });
+        }
+    };
+    User.init({
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        image: DataTypes.STRING,
+        password: {
+            type: DataTypes.STRING,
+            set(value) {
+                const hash = bcrypt.hashSync(value, 10);
+                this.setDataValue('password', hash);
+            }
+        },
+        roleId: DataTypes.INTEGER,
+        deletedAt: DataTypes.DATE
+    }, {
+        sequelize,
+        modelName: 'User',
+        timestamps: true,
+        paranoid: true
+    });
+    return User;
 };
