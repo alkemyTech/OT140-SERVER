fa8cc363b56a1df06e86a3051143e1a2eda2d388
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-04-08 06:59:19 -0300

    delete-slide

--- controllers/slidesController.js
+++ controllers/slidesController.js
@@ -1,42 +1,54 @@
-const Slide = require('../models').Slide;
+const Slide = require("../models").Slide;
 
 const postSlide = async (req, res) => {
-    const { imageUrl, text, order, organizationId } = req.body;
-    const slide = await Slide.create({ imageUrl, text, order, organizationId });
-    res.status(201).json({ msg: "Slide created successfully", slide });
-}
+  const { imageUrl, text, order, organizationId } = req.body;
+  const slide = await Slide.create({ imageUrl, text, order, organizationId });
+  res.status(201).json({ msg: "Slide created successfully", slide });
+};
 
 const updateSlide = async (req, res) => {
-    const { id } = req.params
-    const { imageUrl, text, order, organizationId } = req.body
+  const { id } = req.params;
+  const { imageUrl, text, order, organizationId } = req.body;
 
-    const slide = await Slide.findByPk(id);
+  const slide = await Slide.findByPk(id);
 
-    if (!slide) {
+  if (!slide) {
+    res.status(404).json("Slides doesnt exist");
+  } else {
+    await Slide.update(
+      {
+        imageUrl: imageUrl,
+        text: text,
+        order: order,
+        organizationId: organizationId,
+      },
+      { where: { id } }
+    );
 
-        res.status(404).json('Slides doesnt exist');
-    } else {
+    const updatedSlide = await Slide.findByPk(id);
 
-        await Slide.update({
-            imageUrl: imageUrl,
-            text: text,
-            order: order,
-            organizationId: organizationId
-        },
-            { where: { id } }
-        );
+    res.status(201).json({
+      msg: "Slide Updated",
+      updatedSlide: updatedSlide,
+    });
+  }
+};
+const deleteSlide = async (req, res) => {
+  const { id } = req.params;
 
-        const updatedSlide = await Slide.findByPk(id)
-
-        res.status(201).json({
-            msg: 'Slide Updated',
-            updatedSlide: updatedSlide
-        });
-    }
-}
-
+  const findSlide = await Slide.findByPk(id);
+  if (!findSlide) {
+    res.status(404).json({
+      msg: "Sorry,Silde does not exist",
+    });
+  } else {
+    await Slide.destroy({ where: { id } });
+    res.status(200).json({ msg: "The Slide was deleted" });
+  }
+};
 
 module.exports = {
-    postSlide,
-    updateSlide
+  postSlide,
+  updateSlide,
+  deleteSlide,
 };
--- routes/slides.js
+++ routes/slides.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const Slide = require('../models').Slide;
-const { postSlide, updateSlide } = require('../controllers/slidesController');
+const { postSlide, updateSlide,deleteSlide } = require('../controllers/slidesController');
 
 const router = Router();
 /* GET home page. */
 
 router.post('/', postSlide );
 router.put('/:id', updateSlide );
+router.delete('/:id',deleteSlide)
 
 module.exports = router;
