7b2ec3c1c16cac8aeba6c3628575d27fc3f5f5af
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2022-04-08 20:04:15 -0300

    Minor changes

--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            User.belongsTo(models.Role, { as: 'roleId' });
+            //User.belongsTo(models.Role, { as: 'roleId' });
         }
     };
     User.init({
