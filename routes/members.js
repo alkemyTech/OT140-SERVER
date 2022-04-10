1dec1d360e94fda294a0c36011659e6953a2c1c6
Ricardo <ricardoemanuelracca@gmail.comgit config --global user.email ricardoemanuelracca@gmail.comgit config --global user.email ricardoemanuelracca@gmail.com>
2022-04-10 16:42:27 -0300

    restaure archivo modificado erroneamente

--- routes/members.js
+++ routes/members.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { getAll, create, update,remove } = require('../controllers/members');
 
-// /* GET members listing. */
-// router.get('/', getAll);
+/* GET members listing. */
+router.get('/', getAll);
 
-// /* POST members listing. */
-// router.post('/', create);
-// router.put('/:id',update);
-// router.delete('/:id', remove);
+/* POST members listing. */
+router.post('/', create);
+router.put('/:id',update);
+router.delete('/:id', remove);
 module.exports = router; 
