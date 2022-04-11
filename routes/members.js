31312829facced81fda0bce0ff4cb6187cccf35b
javier1793-op <javier17unt@gmail.com>
2022-04-10 22:19:51 -0300

    main-cambios

--- routes/members.js
+++ routes/members.js
@@ -1,12 +1,2 @@
 const express = require('express');
 const router = express.Router();
-const { getAll, create, update,remove } = require('../controllers/members');
-
-/* GET members listing. */
-router.get('/', getAll);
-
-/* POST members listing. */
-router.post('/', create);
-router.put('/:id',update);
-router.delete('/:id', remove);
-module.exports = router; 
