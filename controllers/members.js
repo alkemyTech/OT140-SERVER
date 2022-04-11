e546cf3abe9cc2d44e58778cd5a6d82eb7467c3f
alesandroo92 <alesandro_92@hotmail.com>
2022-04-11 19:03:30 -0300

    create members was modified

--- controllers/members.js
+++ controllers/members.js
@@ -28,15 +28,22 @@ const getMember = async (req, res) => {
 
 const createMember = async (req, res) => {
   try {
-    const {name} = req.body;
-    const newMember = await member.create({ name: name });
+    const {name, facebookUrl, instagramUrl, linkedinUrl, image, description} = req.body;
+    const newMember = await member.create({ 
+      name: name,
+      facebookUrl: facebookUrl,
+      instagramUrl,
+      linkedinUrl,
+      image,
+      description
+    });
     res.status(200).json({
       msg: `Member was created successfully`,
       data: newMember
   });
     
   } catch (error) {
-      res.status(404).json({error});
+      res.status(500).json({error});
   }
 }
 
