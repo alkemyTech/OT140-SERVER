1640ec34f97fe80b9dedbde04830b5bda84d7d3d
burdiaktri <burdiaktrinidad33@gmail.com>
2022-04-11 15:06:50 -0300

    admin role check added

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router()
 const {getCategoryById, getCategories, updateCategories, deleteCategorie, createCategorie} = require('../controllers/categories.controller')
+const {checkIsAdminLogged} = require('../middlewares/commons')
 
-router.get('/:id', getCategoryById)
+router.get('/:id', checkIsAdminLogged, getCategoryById)
 router.get('/', getCategories)
 router.post('/', createCategorie)
 router.put('/:categorieId', updateCategories)
