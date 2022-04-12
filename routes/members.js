06d411a68723573de10834289c27bb61661c7958
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-04-12 07:02:54 -0300

    add checkIsAdminLogged

--- routes/members.js
+++ routes/members.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAll, create, update,remove } = require('../controllers/members');
+//const { getAll, create, update,remove } = require('../controllers/members');
 
-/* GET members listing. */
+/* GET members listing. 
 router.get('/', getAll);
 
-/* POST members listing. */
+/* POST members listing. 
 router.post('/', create);
 router.put('/:id',update);
-router.delete('/:id', remove);
+router.delete('/:id', remove);*/
 module.exports = router; 
--- routes/slides.js
+++ routes/slides.js
@@ -1,12 +1,16 @@
-const { Router } = require('express');
-const Slide = require('../models').Slide;
-const { postSlide, updateSlide,deleteSlide } = require('../controllers/slidesController');
-
+const { Router } = require("express");
+const Slide = require("../models").Slide;
+const {
+  postSlide,
+  updateSlide,
+  deleteSlide,
+} = require("../controllers/slidesController");
+const { checkIsAdminLogged } = require("../middlewares/commons");
 const router = Router();
 /* GET home page. */
 
-router.post('/', postSlide );
-router.put('/:id', updateSlide );
-router.delete('/:id',deleteSlide)
+router.post("/", postSlide);
+router.put("/:id", updateSlide);
+router.delete("/:id",checkIsAdminLogged, deleteSlide);
 
 module.exports = router;
