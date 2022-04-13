7c5844120ac8564698de2814070cc30a4b41d22f
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2022-04-13 14:11:34 -0300

    First commit. Fixed news.js. No controllers members.js

--- routes/news.js
+++ routes/news.js
@@ -1,15 +1,14 @@
-
 const express = require('express')
 const router = express.Router()
-const {createNews, updateNews, getNew, listNews, deleteNew} = require('../controllers/news')
-const {validateCreateNews} = require('../validator/news')
+const { createNews, updateNews, getNew, listNews, deleteNew } = require('../controllers/news')
+const { validateCreateNews } = require('../validator/news')
 const { newsDeleteForm } = require('../middlewares/validations')
 const { checkToken } = require('../middlewares/check-token')
 const checkRole = require('../middlewares/check-role')
 
 router
     .get("/:id", getNew)
-    .get("/list", listNews );
+    .get("/list", listNews)
     .post('/', validateCreateNews, createNews)
     .put('/:id', updateNews)
     .delete("/:id", newsDeleteForm, deleteNew);
