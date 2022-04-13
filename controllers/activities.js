a47c6b47734b86ed8588a0a911ff82da2494562c
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2022-04-13 20:26:42 -0300

    Completed CRUD in Activities

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,5 +1,56 @@
 const db = require('../models/index');
 
+const getAll = async(req, res) => {
+
+    try {
+        const activities = await db.Activities.findAll({
+            attributes: ['name', 'image', 'content']
+        });
+        res.status(200).json(
+            activities)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Internal server error')
+    }
+};
+
+const getById = async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const activity = await db.Activities.findOne({
+            where: {
+                id
+            },
+        });
+        !activity
+            ?
+            res.status(404).send("Activity not found") :
+            res
+            .status(201)
+            .send({ data: activity, message: "Activity succesfully found" });
+    } catch (err) {
+        res.status(500);
+    }
+};
+
+const createActivity = async(req, res) => {
+    try {
+        const { name, content } = req.body;
+        const activity = await db.Activities.create({
+            name,
+            content
+        })
+        res
+            .status(200)
+            .send({
+                data: activity,
+                message: "Activity succesfully created."
+            });
+
+    } catch (err) {
+        res.status(500);
+    }
+};
 
 const updateActivity = async(req, res) => {
     try {
@@ -15,7 +66,7 @@ const updateActivity = async(req, res) => {
                 name: name,
                 content: content,
             }, {
-                where: { id: id }
+                where: { id }
             })
 
             const activityUpdated = await db.Activities.findByPk(id)
@@ -37,4 +88,9 @@ const updateActivity = async(req, res) => {
     }
 }
 
-module.exports = { updateActivity }
+module.exports = {
+    getAll,
+    getById,
+    createActivity,
+    updateActivity
+}
