b5d8da9985b1d378e5dc46902e7d8db95ea3dcf0
javier1793-op <javier17unt@gmail.com>
2022-04-12 23:45:26 -0300

    middlewares-commons

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,12 +3,13 @@ const express = require("express");
 const { createContact } = require("../controllers/contact.controller");
 const { getContacts } = require('../controllers/contacts');
 const { checkToken} = require('../middlewares/check-token');
+const {checkIsAdminLogged} = require('../middlewares/commons');
 const router = express.Router();
 
 router
     .post("/", createContact);
 
-    router.get('/backoffice', checkToken, getContacts);
+    router.get('/backoffice', checkToken,checkIsAdminLogged, getContacts);
 
 module.exports = router;
 
