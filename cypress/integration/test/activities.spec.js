91905cac3337be2c6b55bea6ae89c67eee04203d
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2022-04-14 13:52:20 -0300

    Added test cases get, getById, getByWrongId, post, activity

--- cypress/integration/test/activities.spec.js
+++ cypress/integration/test/activities.spec.js
@@ -1,22 +1,51 @@
-const url = '://localhost:3000/activities';
+const url = 'http://localhost:3000/activities';
 const id = 1;
-const noId = 404;
+const noId = 999;
 
-describe('Endpoint /activities'), () => {
+const activity = {
+    name: 'TEST Name',
+    content: 'TEST Content'
+}
 
-    it('Get all activities'), () => {
-        cy.request(url)
+describe('Endpoint /activities', () => {
+
+    it('Get all activities', () => {
+        cy.request('GET', url)
             .should((response) => {
                 expect(response.status).to.eq(200)
                 expect(response.body).to.not.be.null
             });
-    };
+    });
 
-    it('Get an activity by Id'), () => {
+    it('Get an activity by Id', () => {
         cy.request(`${url}/${id}`)
             .should((response) => {
                 expect(response.status).to.eq(201)
                 expect(response.body).to.not.be.null
+                expect(response.body).to.have.property('data')
+                expect(response.body).to.have.property('message')
             });
-    };
-}
+    });
+
+    it('Get an activity by wrong Id', () => {
+        cy.request({
+            url: `${url}/${noId}`,
+            failOnStatusCode: false
+        }).should((response) => {
+            expect(response.status).to.eq(404)
+            expect(response.body).to.not.be.null
+        });
+    });
+
+    it('Post a new activity', () => {
+        cy.request({
+                method: 'POST',
+                url,
+                body: activity
+            }).should((response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body).to.not.be.null
+            })
+            .its('body')
+    });
+});
