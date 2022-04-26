03d2d9280aac0f1381f0631ffb16510554470477
alesandroo92 <alesandro_92@hotmail.com>
2022-04-26 08:33:23 -0300

    ownership was modified

--- middlewares/ownership.js
+++ middlewares/ownership.js
@@ -1,22 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../models/");
 
-// Esta funcion se encarga de validar el token de cualquier usuario.
-const checkToken = async(req, res, next) => {
-    const token = req.headers['user-token'];
-    if (!token) return res.status(401).json({ error: 'Need to include user-token on the header' });
-    try {
-        const { uid } = jwt.verify(token, process.env.SECRET);
-        const user = await User.findOne({ where:{ id : uid, deletedAt : null } });
-        if(!user) return res.status(404).json({ msg: 'Invalid token - User not found' });
-        next();        
-    } catch (error) {
-        return res.status(403).json({msg: "Ha sucedido el siguiente error: "+error});
-    }
-}
-
-// Esta funcion se encarga de validar el token solo de los administradores.
-const checkTokenAdmin = async(req, res, next) => {
+const isOwnership = async(req, res, next) => {
     const token = req.headers['user-token'];
     if (!token) return res.status(401).json({ error: 'Need to include user-token on the header' });
     try {
@@ -25,12 +10,12 @@ const checkTokenAdmin = async(req, res, next) => {
         if(!user || user.RoleId !== 1) return res.status(403).json({ msg: 'Usuario no autorizado' });
         next();        
     } catch (error) {
-        return res.status(403).json({msg: "Ha sucedido el siguiente error: "+error});
+        return res.status(500).json({msg: "Ha sucedido el siguiente error: "+error});
     }
 }
 
 
-module.exports = { checkToken, checkTokenAdmin };
+module.exports = { isOwnership };
 
 
 
