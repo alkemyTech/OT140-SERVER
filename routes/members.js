0778ca13a92637517181e7374ce772ebb18ca20c
Maxi Ceballos <maxi@MBP-de-Maxi.fibertel.com.ar>
2022-04-27 00:17:12 -0300

    empate

--- routes/members.js
+++ routes/members.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const { getMember, deleteMember, createMember } = require('../controllers/members');
 const {validateCreateMember} = require("../validator/members");
 
@@ -8,6 +7,5 @@ const {validateCreateMember} = require("../validator/members");
 router.post('/', validateCreateMember, createMember);
 router.get('/', getMember);
 router.delete("/:id", deleteMember);
-
 
 module.exports = router; 
